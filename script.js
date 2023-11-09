fe202acd155f2a71a714867a165bdc5ec3d7ab1d
Luís Campanholi <lpcampanholi@gmail.com>
2023-11-09 15:05:00 -0300

    projeto final

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ async function buscarEMostrarVideos() {
                             <img class="img-canal" src="${video.imagem}" alt="Logo do Canal">
                             <h3 class="titulo-video">${video.titulo}</h3>
                             <p class="titulo-canal">${video.descricao}</p>
+                            <p class="categoria" hidden>${video.categoria}</p>
                         </div>
                     </li>
                     `;
@@ -33,13 +34,37 @@ const barraDePesquisa = document.querySelector(".pesquisar__input");
 
 barraDePesquisa.addEventListener("input", filtrarPesquisa);
 
-function filtrarPesquisa(){
+function filtrarPesquisa() {
   const videos = document.querySelectorAll(".videos__item");
   const valorFiltro = barraDePesquisa.value.toLowerCase();
 
   videos.forEach((video) => {
-    const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
+    const titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
 
-    video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
-  });
+    video.style.display = valorFiltro
+      ? titulo.includes(valorFiltro) ? "block" : "none" : "block";
+      });
+}
+
+// FILTRAR POR CATEGORIAS
+
+const botaoCategoria = document.querySelectorAll(".superior__item");
+
+botaoCategoria.forEach((botao) => {
+  let nomeCategoria = botao.getAttribute("name");
+  botao.addEventListener("click", () => filtrarPorCategoria(nomeCategoria));
+});
+
+function filtrarPorCategoria(filtro) {
+  const videos = document.querySelectorAll(".videos__item");
+  for (let video of videos) {
+    let categoria = video.querySelector(".categoria").textContent.toLowerCase();
+    let valorFiltro = filtro.toLowerCase();
+
+    if (!categoria.includes(valorFiltro) && valorFiltro != "tudo") {
+      video.style.display = "none";
+    } else {
+      video.style.display = "block";
+    }
+  }
 }
